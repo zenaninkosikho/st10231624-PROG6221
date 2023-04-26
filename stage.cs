a3bd4a44a358598fe61a1a15577a9dbe01c44168
ST10231624 Nkosikho Zenani <104991362+zenaninkosikho@users.noreply.github.com>
2023-04-26 13:39:55 +0200

    Add files via upload

---
+++ stage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class stage
+    {
+        public static void Main()
+        {
+            //creates an instance of recipe
+            recipe newRecipe = new recipe();
+            
+            //functions give you each step to deal for recipe
+            newRecipe.writeRecipe();
+            newRecipe.showRecipe();
+            newRecipe.showMenu();
+        }
+    }
+}
